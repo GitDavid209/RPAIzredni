e66221e5432c746dae3142a61faf0f1a16efd87f
Zan Puc <zan.puc@gmail.com>
2023-01-18 19:04:31 +0100

    Lamida Izrazi

--- ElektrikaFunc/ElektrikaFunc/Program.cs
+++ ElektrikaFunc/ElektrikaFunc/Program.cs
@@ -12,14 +12,26 @@ namespace ElektrikaFunc
         {
             ElektrikaEntities1 en = new ElektrikaEntities1();
             //1. izberi čas meritve in skupno moč
-            
+            var x1 = en.Meritve.Select(a => new { a.ZapisČas, moč = a.kW1 + a.kW2 + a.kW2 }).Take(10);
+            foreach (var y in x1)
+            {
+                Console.WriteLine(y.ZapisČas+" "+y.moč);
+            }
             //ista poizvedba z lambda izrazom
             //2. izberi čas meritve in skupno moč za datum 18.8.2013
+            DateTime d= DateTime.Parse("18.8.2013");
+            DateTime d2 = DateTime.Parse("19.8.2013");
+            var x2 = en.Meritve.Where(a => a.ZapisČas >= d && a.ZapisČas<d2);
+            foreach (var y in x2)
+            {
+                Console.WriteLine(y.ZapisČas);
+            }
             //3. izračunaj povprečno moč za datum 18.8.2013
             //4. izračunaj maximalno moč za ta datum
             //5. izračunaj minimalno moč za ta datum
             //6. izračunaj povprečno moč po urah za dan 18.8.2013
             //7. izračunaj 15 minutna povprečja za 18.8.2013
+            Console.ReadLine();
         }
     }
 }
