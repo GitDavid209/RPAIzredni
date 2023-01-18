d0a43968f76ae04728fce4d946a5129c6054d8ba
Zan Puc <zan.puc@gmail.com>
2023-01-18 18:53:11 +0100

    push 18.1.23

---
+++ VajaFuncDelegat/VajaFuncDelegat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaFuncDelegat
+{
+    internal class Program
+    {
+        //delegate string Spremeni(string s);
+        static void Main(string[] args)
+        {
+            Func<string,string> metoda = x=>x.ToUpper();
+            string ime = "Barbara";
+            Console.WriteLine(metoda(ime));
+            Console.ReadLine();
+        }
+
+        //private static string VVelik(string s)
+        //{
+        //    return s.ToUpper();
+        //}
+    }
+}
