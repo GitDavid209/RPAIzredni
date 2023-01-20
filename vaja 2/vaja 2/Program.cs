6119be15ad6e19de51165a5d5b38bb223656ef1f
David Černic <David_Cernic@outlook.com>
2023-01-20 09:48:49 +0100

    Vaja2

---
+++ vaja 2/vaja 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaja_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari Donald farmo ima");
+            Console.WriteLine("IAIAO");
+            Console.WriteLine("Na tej firmi osla dva");
+            Console.WriteLine("IAIAO");
+            Console.ReadLine();
+        }
+    }
+}
